2f6757dbd830b2400b3a28d671f072ff6763ed11
Andrew-zzou <andrewzou04@gmail.com>
2026-01-25 16:39:34 +1300

    added wsgi path for clode deployment.

--- Astop/Astop/settings.py
+++ Astop/Astop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*81*favy@3bp-@ee(ok!tzbr8kba!!@e!yni5nq#_jmgky^*f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'PYTHONPATH' is os.environ:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['.ap-southeast-2.elasticbeanstalk.com']
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = []
+
 
 
 # Application definition
